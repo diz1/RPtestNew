3bc03954373c964a40c6385273a666e47c495d0c
diz1 <dima-smirnov1@mail.ru>
2020-04-10 12:48:56 +0300

    Checkbox fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,6 @@
 //= jqsettings.js
 'use strict';
 
-
 // Переменные и функции
 const btnSave = document.getElementById('btn_save'),
 	warning = document.querySelector('.warning__inner'),
@@ -15,36 +14,75 @@ const btnSave = document.getElementById('btn_save'),
 	responsiblePosition = document.querySelector('#responsible_position'),
 	descriptionInner = document.querySelector('#description_inner'),
 	descriptionPublic = document.querySelector('#description_public'),
-	errorSpan = document.createElement('span');
+	inputs = document.querySelectorAll('.form__input');
 
 const checkReg = (elem, regExp, msg) => {
-		errorSpan.remove();
-		elem.classList.remove('form__input--validate--error');
-		elem.nextElementSibling.classList.remove('form__label--validate--error');
+		console.dir(elem.parentElement);
+		let span = document.createElement('span');
+		span.classList.toggle('validate');
+		span.classList.toggle('validate__error');
+		let lastElem = elem.parentElement.lastElementChild;
+		if (lastElem.tagName.toLowerCase() === 'span') {
+			lastElem.remove();
+		}
+		if (elem.classList.contains('form__input--validate--error')) {
+			elem.classList.toggle('form__input--validate--error');
+		}
+		if (elem.nextElementSibling.classList.contains('form__label--validate--error')) {
+			elem.nextElementSibling.classList.toggle('form__label--validate--error');
+		}
 		if (regExp.test(elem.value) === false) {
-			elem.classList.add('form__input--validate--error');
-			elem.nextElementSibling.classList.add('form__label--validate--error');
-			elem.parentElement.append(errorSpan);
-			errorSpan.classList.add('validate');
-			errorSpan.classList.add('validate__error');
-			errorSpan.textContent = msg;
+			elem.classList.toggle('form__input--validate--error');
+			elem.nextElementSibling.classList.toggle('form__label--validate--error');
+			elem.parentElement.append(span);
+			span.textContent = msg;
 			return false;
 		}
 	},
 	checkEmpty = elem => {
 		let msg = 'Поле пустое, введите символы';
-		errorSpan.remove();
-		elem.classList.remove('form__input--validate--error');
-		elem.nextElementSibling.classList.remove('form__label--validate--error');
+		let span = document.createElement('span');
+		span.classList.toggle('validate');
+		span.classList.toggle('validate__error');
+		let lastElem = elem.parentElement.lastElementChild;
+		if (lastElem.tagName.toLowerCase() === 'span') {
+			lastElem.remove();
+		}
+		if (elem.classList.contains('form__input--validate--error')) {
+			elem.classList.toggle('form__input--validate--error');
+		}
+		if (elem.nextElementSibling.classList.contains('form__label--validate--error')) {
+			elem.nextElementSibling.classList.toggle('form__label--validate--error');
+		}
 		if (elem.value === '') {
-			elem.classList.add('form__input--validate--error');
-			elem.nextElementSibling.classList.add('form__label--validate--error');
-			elem.parentElement.append(errorSpan);
-			errorSpan.classList.add('validate');
-			errorSpan.classList.add('validate__error');
-			errorSpan.textContent = msg;
+			elem.classList.toggle('form__input--validate--error');
+			elem.nextElementSibling.classList.toggle('form__label--validate--error');
+			elem.parentElement.append(span);
+			span.textContent = msg;
 			return false;
 		}
+	},
+	checkInputValues = () => {
+		inputs.forEach(item => {
+			item.addEventListener('input', () => {
+				if (item.type !== 'checkbox') {
+					if (item.value !== '') {
+						btnSave.removeAttribute('disabled');
+					} else {
+						btnSave.setAttribute('disabled', 'disabled');
+					}
+				}
+			});
+		});
+	},
+	checkInputValuesOnLoad = () => {
+		inputs.forEach(item => {
+			if (item.type !== 'checkbox') {
+				if (item.value !== '') {
+					btnSave.removeAttribute('disabled');
+				}
+			}
+		});
 	},
 	validMail = () => {
 		let regExp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
@@ -53,7 +91,7 @@ const checkReg = (elem, regExp, msg) => {
 	},
 	validAlias = () => {
 		let regExp = /^[A-Za-z0-9-]+$/,
-			msg = 'Алиас не может быть пустым или содержать русские буквы и пробелы';
+			msg = 'Алиас не может быть пустым, содержать русские буквы, пробелы или символы';
 		checkReg(groupAlias, regExp, msg);
 	},
 	saveData = () => {
@@ -80,9 +118,11 @@ const checkReg = (elem, regExp, msg) => {
 		e.preventDefault();
 		saveData();
 		warning.classList.toggle('warning__inner--active');
+		btnSave.setAttribute('disabled', 'disabled');
 		setTimeout(() => {
 			warning.classList.toggle('warning__inner--active');
-		}, 5500);
+			setTimeout(() => btnSave.removeAttribute('disabled'), 500);
+		}, 4500);
 	},
 	showPublic = () => publicDescription.classList.toggle('form-description__item--active'),
 	validName = () => {
@@ -101,19 +141,21 @@ const checkReg = (elem, regExp, msg) => {
 		checkEmpty(descriptionPublic);
 	};
 
-
 // Вызовы функций
-getData();
-
+document.addEventListener('DOMContentLoaded', () => {
+	getData();                              // Получаем данные при загрузке страницы
+	checkInputValuesOnLoad();               // Проверяем заполненность полей при загрузке для включения кнопки сохранения
+	checkInputValues();                     // Проверяем заполненность полей для включения кнопки сохранения
+});
 
 // Обработчики
 btnSave.addEventListener('click', showWarning);
-checkboxPublicName.addEventListener('change', showPublic);
-groupAlias.addEventListener('change', validAlias);
-groupMail.addEventListener('change', validMail);
-groupName.addEventListener('change', validName);
-responsibleName.addEventListener('change', validResponsibleName);
-responsiblePosition.addEventListener('change', validResponsiblePosition);
-descriptionInner.addEventListener('change', validDescriptionInner);
-descriptionPublic.addEventListener('change', validDescriptionPublic);
+checkboxPublicName.addEventListener('input', showPublic);
+groupAlias.addEventListener('input', validAlias);
+groupMail.addEventListener('input', validMail);
+groupName.addEventListener('input', validName);
+responsibleName.addEventListener('input', validResponsibleName);
+responsiblePosition.addEventListener('input', validResponsiblePosition);
+descriptionInner.addEventListener('input', validDescriptionInner);
+descriptionPublic.addEventListener('input', validDescriptionPublic);
 
